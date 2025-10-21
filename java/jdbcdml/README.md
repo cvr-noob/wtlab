30d03c6598380df693e16d3b8b26b9425f7935e7
cvr-noob <abc@google.com>
2025-10-21 23:13:54 +0530

    .

---
+++ java/jdbcdml/README.md
@@ -0,0 +1,9 @@
+# Clean & Install
+```bash
+mvn clean install
+```
+
+# Run
+```bash
+mvn exec:java "-Dexec.mainClass=com.example.App"
+```
---
+++ java/jdbcdml/src/main/java/com/example/App.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Connection conn = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/wtdb", "root", "password"
+        );
+
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("INSERT INTO users (name, email) VALUES ('CVR', 'cvr@cvr.ac.in')");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+        while(rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            String email = rs.getString("email");
+            
+            System.out.println(id + " | " + name + " | " + email);
+        }
+    }
+}
